80b92dcc065070c16e60970f213edd2f057dd80f
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2022-11-15 08:41:46 -0300

    chore: remove unnecessary files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const container = document.getElementById('root');
+
+createRoot(container as HTMLElement).render(<App />);
